e87f7a4108f9479fc279334be63178480771d012
Vladimir Sergeev <Vladimir_Sergeev1@epam.com>
2018-04-17 19:56:14 +0300

    kv-framework-20: Cleanup test to REF object https://github.com/aboudnik/kv-framework/issues/20

--- src/test/java/org/boudnik/framework/test/testsuites/CreateSaveRefTest.java
+++ src/test/java/org/boudnik/framework/test/testsuites/CreateSaveRefTest.java
@@ -3,6 +3,7 @@ package org.boudnik.framework.test.testsuites;
 import org.boudnik.framework.Transaction;
 import org.boudnik.framework.test.core.RefTestEntry;
 import org.boudnik.framework.test.core.TestEntry;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,50 +13,6 @@ public class CreateSaveRefTest {
         Transaction.instance().withCache(RefTestEntry.class, TestEntry.class);
     }
 
-//    @Test
-//    public void testCreateSaveCommit2() {
-//        Transaction tx = Transaction.instance();
-//        tx.txCommit(() -> {
-//            TestEntry te2 = new TestEntry("testCreateSaveCommit2");
-//            TestEntry te3 = new TestEntry("testCreateSaveCommit3");
-//            OBJ.Implementation impl = new ImplObject("impl");
-//            OBJ.Implementation.REF ref = impl.new REF(TestEntry.class);
-//            ref.set(te2);
-//            System.out.println("REF: " + ref);
-//            te2.save();
-//            te3.save();
-//        });
-//
-//        TestEntry te2 = tx.getAndClose(TestEntry.class, "testCreateSaveCommit2");
-//        TestEntry te3 = tx.getAndClose(TestEntry.class, "testCreateSaveCommit3");
-//        Assert.assertNotNull(te2);
-//        Assert.assertNotNull(te3);
-//        OBJ.Implementation.REF ref = new ImplObject("impl").new REF(TestEntry.class);
-//        ref.set(te2);
-//        System.out.println(ref.get());
-//    }
-
-/*
-    @Test
-    public void testCreateSaveCommit() {
-        Transaction tx = Transaction.instance();
-        TestEntry testEntry = new TestEntry("test");
-        OBJ.Implementation<String>.REF<String, TestEntry> ref = new OBJ.Implementation<String>("test"){}.new REF<String, TestEntry> (TestEntry.class);
-        ref.set(testEntry);
-        RefTestEntry refTestEntry = new RefTestEntry(ref);
-        refTestEntry.getKey();
-        tx.txCommit(refTestEntry);
-
-        RefTestEntry refTestEntry1 = tx.getAndClose(RefTestEntry.class, refTestEntry.getKey());
-
-        Assert.assertNotNull(refTestEntry1);
-        Assert.assertNotNull(refTestEntry1.getKey());
-        System.out.println(refTestEntry.getKey()); //<-reference = testEntry
-        System.out.println(refTestEntry1.getKey()); //<-reference = null
-        Assert.assertEquals(refTestEntry.getKey().get(), testEntry);
-        Assert.assertNotNull(refTestEntry1.getKey().get()); //<-NPE
-    }
-*/
     @Test
     public void testCreateSaveCommit() {
         Transaction tx = Transaction.instance();
@@ -67,9 +24,10 @@ public class CreateSaveRefTest {
             ref[0] = new RefTestEntry(1, entry);
             ref[0].save();
         });
-        tx.txCommit(() -> {
-            TestEntry entry = ref[0].ref.get();
-            System.out.println("entry = " + entry);
-        });
+        RefTestEntry refTestEntry = tx.get(RefTestEntry.class, ref[0].getKey());
+        TestEntry test = tx.get(TestEntry.class, "test");
+        Assert.assertNotNull(refTestEntry);
+        Assert.assertNotNull(test);
+        Assert.assertEquals(refTestEntry.getRef().get().getKey(), test.getKey());
     }
 }
