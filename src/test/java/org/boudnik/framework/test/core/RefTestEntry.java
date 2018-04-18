0923816ef6bb2c3db68320fb87bddb941e04f9d6
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-04-17 20:58:11 -0400

    kv-framework-29 stale REF
    fixed: check out REF.get() and test. Also fixed some generics

--- src/test/java/org/boudnik/framework/test/core/RefTestEntry.java
+++ src/test/java/org/boudnik/framework/test/core/RefTestEntry.java
@@ -2,26 +2,19 @@ package org.boudnik.framework.test.core;
 
 import org.boudnik.framework.OBJ;
 
-import java.util.HashMap;
-import java.util.Map;
+public class RefTestEntry extends OBJ.Implementation<Integer> {
+    private final REF<String, TestEntry> ref = new REF<>(TestEntry.class);
 
-public class RefTestEntry implements OBJ<Integer> {
-    Map<Integer, String> m = new HashMap<>();
-
-    private final Integer key;
-    private final OBJ.REF<String, TestEntry> ref = new OBJ.REF<>(TestEntry.class);
+    private RefTestEntry(Integer key) {
+        super(key);
+    }
 
     public RefTestEntry(Integer key, TestEntry test) {
-        this.key = key;
-        this.ref.set(test);
+        this(key);
+        ref.set(test);
     }
 
-    public REF<String, TestEntry> getRef() {
-        return ref;
-    }
-
-    @Override
-    public Integer getKey() {
-        return key;
+    public TestEntry getEntry() {
+        return ref.get();
     }
 }
