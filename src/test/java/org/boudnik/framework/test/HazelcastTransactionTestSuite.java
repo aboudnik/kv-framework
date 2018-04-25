4119fbe9527c178b37a231bd91b2ca9b89585559
Vladimir Sergeev <Vladimir_Sergeev1@epam.com>
2018-04-25 12:33:02 +0300

    kv-framework-70: Added possibility to work with packages in suites. Quick fix on RollbackTest done https://github.com/aboudnik/kv-framework/issues/70

--- src/test/java/org/boudnik/framework/test/HazelcastTransactionTestSuite.java
+++ src/test/java/org/boudnik/framework/test/HazelcastTransactionTestSuite.java
@@ -1,29 +1,13 @@
 package org.boudnik.framework.test;
 
-import org.boudnik.framework.test.testsuites.*;
+import com.googlecode.junittoolbox.SuiteClasses;
+import com.googlecode.junittoolbox.WildcardPatternSuite;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        CreateDeleteTest.class,
-        CreateSaveDeleteTest.class,
-        CreateSaveRefTest.class,
-        CreateSaveTest.class,
-        GetDeleteComplexEntryTest.class,
-        GetDeleteTest.class,
-        GetUpdateSaveComplexEntryTest.class,
-        GetUpdateSaveDeleteComplexEntryTest.class,
-        GetUpdateSaveDeleteTest.class,
-        GetUpdateSaveTest.class,
-        MultipleClassesWithSameKeyTest.class,
-        SaveDeleteComplexRefTest.class,
-        SaveDeleteRefTest.class,
-        SaveGetComplexRefTest.class,
-        SaveGetRefTest.class,
-        ObjSaveTest.class,
-        NullSaveTest.class,
+@RunWith(WildcardPatternSuite.class)
+@SuiteClasses({"testsuites/*Test.class",
+        "*Test.class",
 })
 public class HazelcastTransactionTestSuite {
 
