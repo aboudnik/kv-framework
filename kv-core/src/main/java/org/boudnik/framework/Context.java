e8312444ce51828069d8415d7646175ee672c96a
snuyanzin <snuyanzin@gmail.com>
2018-04-27 12:17:53 +0300

    kv-framework-86 add bulk remove for commit

--- kv-core/src/main/java/org/boudnik/framework/Context.java
+++ kv-core/src/main/java/org/boudnik/framework/Context.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Alexandre_Boudnik
@@ -18,7 +19,7 @@ import java.util.Set;
 public abstract class Context implements AutoCloseable {
     private final Map<Class<? extends OBJ>, Map<Object, OBJ>> scope = new HashMap<>();
     private final Set<OBJ> deleted = new HashSet<>();
-    protected Map<Class, BeanInfo> meta = new HashMap<>();
+    protected final Map<Class, BeanInfo> meta = new HashMap<>();
     protected final Map<Object, Object> mementos = new HashMap<>();
 
     public abstract <K, V extends OBJ> V get(Class<V> clazz, K identity);
