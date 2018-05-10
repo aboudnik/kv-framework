f90f1d0d302586450e4fff541122db4ff87463b5
Narek Karapetian <narek_karapetian@epam.com>
2018-05-10 18:23:50 +0300

    store PreparedStatements in the Map and close them and connection in close method

--- kv-tests/src/test/java/org/boudnik/framework/test/H2ContextTestSuite.java
+++ kv-tests/src/test/java/org/boudnik/framework/test/H2ContextTestSuite.java
@@ -2,7 +2,9 @@ package org.boudnik.framework.test;
 
 import com.googlecode.junittoolbox.SuiteClasses;
 import com.googlecode.junittoolbox.WildcardPatternSuite;
+import org.boudnik.framework.Context;
 import org.boudnik.framework.test.testsuites.TransactionTest;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
@@ -15,5 +17,10 @@ public class H2ContextTestSuite {
     public static void beforeAll() {
         TransactionTest.setProvider("H2");
         Initializer.initH2();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        Context.instance().close();
     }
 }
